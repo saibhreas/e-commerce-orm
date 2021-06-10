1821904a876fbcbb760dc935da17f311eef8730d
siobhan <sr@saibhreas.com>
2021-06-09 22:53:33 -0400

    cannot figure out jaws for heroku

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,22 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
